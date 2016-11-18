f5e1e4ffbe1702c308751e19e48f73c8d2eda933
Lisa J Spell <mjspell@gmail.com>
2016-11-18 16:16:36 -0500

    http req tests

--- src/app/http-req/http-req.component.ts
+++ src/app/http-req/http-req.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
-  selector: 'app-http-req',
+  selector: 'http-req',
   templateUrl: './http-req.component.html',
   styleUrls: ['./http-req.component.scss']
 })
 export class HttpReqComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: Http) { 
+    var jsonph = "https://jsonplaceholder.typicode.com";
+    var scdev = "http://localhost.com/sitecore/-/item/v1/?sc_itemid={754AE32E-1082-4167-B5CA-F3A996D06420}";
+    this.http.get(scdev).subscribe(res => {
+      // console.log(res);
+    });
+
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
+  }
 
   ngOnInit() {
   }
